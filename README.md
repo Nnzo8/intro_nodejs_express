a7cddc3105ce9c1c0af7d9b796eeb9329725ae21
Niño Rollane D. Ocliasa <ninorollaneocliasa684@gmail.com>
2025-02-05 23:05:35 +0800

    Updated all

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is an introduction to node and express by Niño Rollane D. Ocliasa
+This repository is an introduction to node and express js.
+
--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const items = ['Apple', 'Banana', 'Orange'];
 
-app.use(express.static('public'));
+app.use(express.json()); // MIDDLEWARE
+
+app.get('/items', (req, res) => {
+    res.json(items);   
+});
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    if (newItem) {
+        items.push(newItem);
+        res.json(items);
+    } else {
+        res.status(400).send('Item is required');
+    }
+});
+
+app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
-app.get('/about',(req, res)=>{
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.get('/about', (req, res) => {
     res.send('About us');
 });
 
-app.use(express.json()); //MIDDLEWARE
-
-app.post('/submit',(req, res)=>{
+app.post('/submit', (req, res) => {
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
 });
 
-app.use((req, res, next)=>{
-    console.log(`${req.method} ${req.url}`);
-    next();
-}); 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
